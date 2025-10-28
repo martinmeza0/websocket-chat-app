9ae0649af38c8ba64a5985b9750cdaa85e064028
Martin Meza <martinmezacorreo@gmail.com>
2025-10-28 10:09:16 -0300

    integrate socket.io
    replace ws with socket.io in the server
    add socket.io script in app/html
    add socket.io in app.js
    add nodemon to auto load the server when there's changes

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
-const socket = new WebSocket("ws://localhost:3000");
+const socket = io("ws://localhost:3000");
 
 function sendMessage(e) {
   e.preventDefault();
   const messageInput = document.getElementById("message-input");
 
   if (messageInput.value) {
-    socket.send(messageInput.value);
+    socket.emit('message', messageInput.value);
     messageInput.value = "";
   }
 
@@ -15,7 +15,7 @@ function sendMessage(e) {
 document.querySelector("form").addEventListener("submit", sendMessage);
 
 // Listen for messages
-socket.addEventListener("message", ({ data }) =>  {
+socket.on("message", (data) =>  {
     const message = document.createElement("li");
     message.textContent = data;
     document.getElementById("messages").appendChild(message);
--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
-const ws = require('ws');
-const server = new ws.Server({ port: '3000' });
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-server.on('connection', socket => {
-  socket.on('message', message => {
-    console.log(`Received: ${message}`);
-    socket.send(`${message}`);
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+  cors: {
+    origin:
+      process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`A user connected: ${socket.id}`);
+
+  socket.on("message", (data) => {
+    console.log(data);
+    io.emit('message', `${socket.id}: ${data}`); //emit this message to everyone connected to the server 
   });
+});
+
+httpServer.listen(3000, () => {
+  console.log("Server is listening on port 3000");
 });
